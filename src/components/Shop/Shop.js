f3405996dab21a2cf23bcc97c3afa9c94ad64cd9
chandan-chakma <cchakma19@gmail.com>
2022-03-26 15:00:37 +0600

    add shop component

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className='products'>
+                <h1>product</h1>
+
+            </div>
+            <div className='cart'>
+                <h2>cart short</h2>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default Shop;
