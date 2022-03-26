9eb783afc42d3d78faf76cb68ebd2b66d0afb072
chandan-chakma <cchakma19@gmail.com>
2022-03-26 17:15:37 +0600

    cart button add

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,12 +3,29 @@ import './Shop.css'
 import Product from './../Product/Product';
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('product.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
 
+    }, []);
+
+    const addToCart = (product) => {
+        console.log('click')
+    }
 
     return (
         <div className='shop-container'>
             <div className='products'>
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        addToCart={addToCart}
 
+                    ></Product>)
+                }
 
             </div>
             <div className='cart'>
